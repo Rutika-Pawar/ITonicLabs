97b58ca504348f8e13242012212cb7fc1820e77f
rutikadattatrayapawar <rutikapawar222@gmail.com>
2023-09-07 19:53:00 +0530

    modified files

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,10 +3,11 @@ import Typed from 'react-typed';
 import { FaPhone } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
+import '../styles/Hero.css';
+
 const Hero = () => {
-  
   const [showContactText, setShowContactText] = useState(false);
-  
+
   const handleContactClick = () => {
     setShowContactText(!showContactText);
     console.log('Contact button clicked');
@@ -54,7 +55,7 @@ const Hero = () => {
   const handleCloseFeedback = () => {
     setIsFeedbackOpen(false);
   };
- 
+
   const ratingLabels = ["Poor", "Fair", "Good", "Great", "Excellent"];
   const [buttonClicked, setButtonClicked] = useState(false);
 
@@ -62,21 +63,22 @@ const Hero = () => {
     setButtonClicked(true);
     // Add your custom behavior for the button click here
   };
+
   return (
-    <div className='text-white' style={{ backgroundColor: '#002244' }}>
-      <div className='max-w-[1000px] h-screen flex flex-col justify-center p-10 mt-[-50px] '>
-        <p className='text-[#00df9a] md:text-2xl font-bold mb-30'>
+    <div className='hero-container'>
+      <div className='hero-content'>
+        <p className='education-text'>
           Transforming education through technology.
         </p>
-        <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-4'>
+        <h1 className='develop-text'>
           You think..We develop..!!
         </h1>
-        <div className='flex items-center'>
-          <p className='md:text-5xl sm:text-4xl text-xl font-bold'>
+        <div className='software-text'>
+          <p className='providing-text'>
             Providing software
           </p>
           <Typed
-            className='md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2'
+            className='products-services-text'
             strings={['Products', 'Services']}
             typeSpeed={120}
             backSpeed={140}
@@ -84,48 +86,26 @@ const Hero = () => {
           />
         </div>
         <Link to='/launches'>
-      <button
-        className='bg-[#00df9a] hover:bg-[#0ab883] text-white font-bold py-3 px-6 rounded mt-20 transition duration-300 ease-in-out transform hover:scale-105 shadow-md'
-        onClick={handleButtonClick}
-      >
-        Upcoming Launches
-      </button>
-    </Link>
+          <button
+            className='launches-button'
+            onClick={handleButtonClick}
+          >
+            Upcoming Launches
+          </button>
+        </Link>
       </div>
-      
+
       {/* Add a container for the image */}
-<div
-   className='h-screen flex items-start justify-end' // Change 'items-center' to 'items-start' and 'justify-center' to 'justify-end'
-   style={{ flex: 1, marginTop: '-600px' }}
->
-  <img
-    src='https://inizsoft.com/wp-content/uploads/2021/02/wordpress-Development-Company-in-India1.gif'
-    alt='Training and Application'
-    style={{ maxWidth: '100%', maxHeight: '80%' }}
-  />
-</div>
+      <div className='image-container'>
+        <img
+          src='https://inizsoft.com/wp-content/uploads/2021/02/wordpress-Development-Company-in-India1.gif'
+          alt='Training and Application'
+        />
+      </div>
       <Link to="/contact" style={{ textDecoration: 'none' }}>
         <button
           onClick={handleContactClick}
-          style={{
-            backgroundColor: '#00df9a', 
-            color: 'white',
-            borderRadius: '100px 0 0 100px', 
-            width: showContactText ? '120px' : '60px', 
-            height: '50px', 
-            position: 'fixed',
-            bottom: '20px', 
-            right: '0', 
-            zIndex: 999,
-            border: 'none',
-            cursor: 'pointer',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'flex-start', 
-            overflow: 'hidden',
-            boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
-            transition: 'width 0.3s',
-          }}
+          className='contact-button'
         >
           <FaPhone style={{ fontSize: '24px', marginLeft: '20px' }} />
           {showContactText && (
@@ -135,94 +115,62 @@ const Hero = () => {
       </Link>
       <a
         onClick={handleFeedbackToggle}
-        style={{
-          color: 'rgb(255, 255, 255)',
-          textShadow: 'rgba(0, 0, 0, 0.25) 0px -1px 0px',
-          textDecoration: 'none',
-          boxShadow: 'none',
-          display: 'block',
-          cursor: 'pointer',
-          backgroundColor: 'rgb(0, 223, 154)',
-          backgroundSize: '100% 100%',
-          backgroundRepeat: 'repeat-x',
-          backgroundPosition: '0px 0px',
-          borderRadius: '5px 5px 0px 0px',
-          padding: '12px 14px',
-          fontSize: '14px',
-          fontWeight: 'bold',
-          position: 'fixed',
-          top: '50%',
-          marginTop: '-22.5px',
-          transform: 'rotate(-90deg)',
-          zIndex: 999,
-          right: '-26.5px',
-          left: 'auto',
-        }}
+        className='feedback-button'
       >
-     Feedback
+        Feedback
       </a>
-      
+
       {isFeedbackOpen && (
-        <div
-          id='feedback-section'
-          className='fixed inset-0 flex items-center justify-center bg-[rgba(0, 0, 0, 0.5)] z-50  backdrop-blur-md'
-        >
-          <div className='bg-[#002244]  border border-white p-6 rounded-lg relative'>
+        <div className='feedback-section'>
+          <div className='feedback-content'>
             <button
               onClick={handleCloseFeedback}
-              className='absolute top-0 right-0 p-2 text-white hover:text-gray-700'
+              className='close-button'
             >
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                width='16'
-                height='16'
-                fill='currentColor'
-                className='bi bi-x'
-                viewBox='0 0 16 16'
-              >
-                <path d='M.879 0a.879.879 0 0 1 1.243 0L8 6.757 13.878.878a.879.879 0 0 1 1.243 1.243L9.243 8l5.878 5.878a.879.879 0 0 1-1.243 1.243L8 9.243l-5.878 5.878a.879.879 0 0 1-1.243-1.243L6.757 8 .879 2.122A.879.879 0 0 1 .879 0z' />
-              </svg>
+              Close
             </button>
-            <h2 className='text-xl font-semibold mb-2'>Feedback</h2>
-            <p className='text-sm mb-4'>We value your input</p>
+            <h2 className='feedback-title'>Feedback</h2>
+            <p className='feedback-description'>We value your input</p>
             <form onSubmit={handleSubmit}>
-              <div className='mb-4'>
-                <label className='block text-white text-sm font-bold mb-2'>
+              <div className='form-group'>
+                <label htmlFor='name' className='label-text'>
                   Name
                 </label>
                 <input
-                  className='w-full px-3 py-2 border rounded-md shadow-sm'
                   type='text'
+                  id='name'
+                  className='input-field'
                   placeholder='Your Name'
                   value={feedbackData.name}
                   onChange={handleNameChange}
                 />
                 {errors.name && (
-                  <p className='text-red-500 text-sm'>{errors.name}</p>
+                  <p className='error-text'>{errors.name}</p>
                 )}
               </div>
-              <div className='mb-4'>
-                <label className='block text-white text-sm font-bold mb-2'>
+              <div className='form-group'>
+                <label htmlFor='feedback' className='label-text'>
                   Feedback
                 </label>
                 <textarea
-                  className='w-full px-3 py-2 border rounded-md shadow-sm'
+                  id='feedback'
+                  className='textarea-field'
                   rows='4'
                   placeholder='Your Feedback'
                   value={feedbackData.feedback}
                   onChange={handleFeedbackChange}
                 />
                 {errors.feedback && (
-                  <p className='text-red-500 text-sm'>{errors.feedback}</p>
+                  <p className='error-text'>{errors.feedback}</p>
                 )}
               </div>
-              <div className='mb-4'>
-                <label className='block text-white text-sm font-bold mb-2'>
+              <div className='form-group'>
+                <label className='label-text'>
                   Rating
-                  </label>
-                <div>
+                </label>
+                <div className='rating-options'>
                   {ratingLabels.map((label, index) => (
-                    <label key={index} className='inline-block mr-2'>
+                    <label key={index} className='rating-label'>
                       <input
                         type='radio'
                         value={index + 1}
@@ -235,7 +183,7 @@ const Hero = () => {
                 </div>
               </div>
               <button
-                className='bg-[#00df9a] text-white py-2 px-4 rounded-md hover:bg-[#00bf87] focus:outline-none focus:shadow-outline'
+                className='submit-button'
                 type='submit'
               >
                 Submit
--- src/components/launches.jsx
+++ src/components/launches.jsx
@@ -2,10 +2,12 @@ import React, { useState } from 'react';
 import screenshot1 from '../assets/screenshot1.png';
 import screenshot2 from '../assets/screenshot2.png';
 import screenshot3 from '../assets/screenshot3.png';
+import screenshot4 from '../assets/screenshot4.png';
 
 const App = () => {
   const [showWelcome, setShowWelcome] = useState(true);
   const [showContent, setShowContent] = useState(false);
+  const [showLearnMore, setShowLearnMore] = useState(false); // New state for Learn More content
 
   const closeWelcome = () => {
     setShowWelcome(false);
@@ -15,6 +17,9 @@ const App = () => {
     setShowContent(!showContent);
   };
 
+  const toggleLearnMore = () => {
+    setShowLearnMore(!showLearnMore); // Toggle the Learn More content
+  };
   const styles = `
     /* Add your CSS styles here */
     .app-container {
@@ -34,31 +39,56 @@ const App = () => {
     }
 
     .info {
-      font-size: 16px;
-      margin-bottom: 10px;
+      padding-bottom: 10px; /* Adjust the padding to control the spacing between text and line */
+      margin-bottom: 20px; /* Adjust the margin to control the spacing between info blocks */
     }
-
+    
+    .info span {
+      border-bottom: 1px solid #002244; /* You can adjust the color as needed */
+      padding-bottom: 2px; /* Adjust the padding to control the spacing between text and line */
+    }
+    
+    .info-devices {
+      font-size: 16px;
+      margin-bottom: 20px;
+    }
     .screenshots {
       display: flex;
       flex-wrap: wrap;
       margin-bottom: 20px;
+      margin-up: 20px;
     }
-
+    
     .screenshot {
       max-width: 200px;
-      margin-right: 10px;
-      margin-bottom: 10px;
+      margin-right: 20px;
+      margin-bottom: 20px;
+      margin-up: 50px;
+      transition: transform 0.3s ease, box-shadow 0.3s ease; /* Add transitions for both transform and box-shadow */
+      transform-origin: center bottom; /* Set the transform origin to center bottom */
+      box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2); /* Add a shadow to the image */
+    }
+    
+    /* Apply a scale transform on hover and increase the shadow */
+    .screenshot:hover {
+      transform: scale(1.05) translateY(5px); /* Slightly down and zoom-in effect */
+      box-shadow: 0 8px 16px rgba(0, 0, 0, 0.3); /* Increase the shadow on hover */
     }
 
     .about-app,
-    .features {
-      font-size: 18px;
-      margin-bottom: 20px;
-    }
+.features {
+  font-size: 18px;
+  margin-bottom: 20px;
+  max-width: 1000px; /* Set a maximum width for the text container */
+}
 
-    .footer {
-      font-size: 14px;
-    }
+.footer {
+  font-weight: bold;
+  font-size: 16px; /* You can adjust the font size as needed */
+  margin-bottom: 20px;
+  max-width: 1000px;
+}
+
 
     .welcome-box {
       background-color: lightblue;
@@ -75,7 +105,10 @@ const App = () => {
       float: right;
       font-weight: bold;
     }
-
+    . close-content-button {
+      cursor: pointer;
+      font-weight: bold;
+    }
     .impressive-box {
       background-color: white;
       padding: 20px;
@@ -94,7 +127,7 @@ const App = () => {
     }
 
     .check-out-button {
-      background-color: #00df9a;
+      background-color: #002244;
       color: white;
       padding: 10px 20px;
       border: none;
@@ -105,11 +138,25 @@ const App = () => {
     }
 
     .check-out-button:hover {
-      background-color: #00987a;
+      background-color: #0033A0;
     }
     
     .impressive-box.show-content {
       transform: translateY(-20px);
+    }
+    .learn-more-button {
+      background-color: #006B7F; /* Background color for the button */
+      color: white;
+      padding: 10px 20px;
+      border: none;
+      border-radius: 5px;
+      cursor: pointer;
+      font-weight: bold;
+      transition: background-color 0.3s ease;
+    }
+    
+    .learn-more-button:hover {
+      background-color: #005267; /* Background color on hover */
     }
   `;
 
@@ -129,9 +176,9 @@ const App = () => {
     <div className={`impressive-box ${showContent ? 'show-content' : ''}`}>
       <span className="impressive-header">BRAINBOOSTERJR</span>
       {showContent ? (
-        <span className="close-content-button" onClick={toggleContent}>
-          X
-        </span>
+      <span className="close-content-button" onClick={toggleContent}>
+      X
+    </span>
       ) : (
         <button className="check-out-button" onClick={toggleContent}>
           Check Out
@@ -141,39 +188,41 @@ const App = () => {
        
 {showContent && (
     <div>
-      <div className="info">
-        BrainBoosterJr Team
-        <br />
-        No ads • Free to use
-      </div>
-      <div className="info">
-        4.5 stars • 2.5M reviews
-        <br />
-        10M+ Downloads • Highly rated
-      </div>
-      <div className="info">
-        Content rating
-        <br />
-        Rated for all ages
-      </div>
+   <div className="info">
+  <span>BrainBoosterJr Team</span>
+  <br />
+  No ads • Free to use
+</div>
+<div className="info">
+  <span>4.5 stars • 2.5M reviews</span>
+  <br />
+  10M+ Downloads • Highly rated
+</div>
+<div className="info">
+  <span>Content rating</span>
+  <br />
+  Rated for all ages
+</div>
+
+
       <div className="info-devices">
         This app is available for all of your devices
       </div>
       <div className="screenshots">
-      {/* Add screenshot images here */}
-      {[screenshot1, screenshot2, screenshot3].map((image, index) => (
-        <img
-          key={index}
-          className="screenshot"
-          src={image}
-          alt={`Screenshot ${index + 1}`}
-          style={{ margin: '30px' }} 
-        />
-      ))}
-    </div>
+  {/* Add screenshot images here */}
+  {[screenshot1, screenshot2, screenshot3, screenshot4].map((image, index) => (
+    <img
+      key={index}
+      className="screenshot"
+      src={image}
+      alt={`Screenshot ${index + 1}`}
+    />
+  ))}
+</div>
+
 
       <div className="about-app">
-        <h2>About this app</h2>
+        <h2>About this app :</h2>
         <p>
           BrainBoosterJr is an educational app designed to enhance children's learning experience through interactive lessons and engaging activities.
         </p>
@@ -185,16 +234,33 @@ const App = () => {
         </p>
       </div>
       <div className="footer">
-        BrainBoosterJr is committed to providing a safe and educational environment for children. We do not display ads and offer free access to our content. Learn more about our privacy policy and terms of use:
+        BrainBoosterJr is committed to providing a safe and educational environment for children. We do not display ads and offer free access to our content. 
+      
+      </div>
+        {/* Learn More button */}
+        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
+            <button className="learn-more-button" onClick={toggleLearnMore}>
+              Learn More
+            </button>
+          </div>
+
+          {/* Learn More content */}
+          {showLearnMore && (
+            <div className="learn-more-content">
+              {/* Add your Learn More content here */}
+              <p>Learn more about our privacy policy and terms of use:
         <br />
         Privacy policy: <a href="https://www.brainboosterjr.com/privacy-policy">Privacy Policy</a>
         <br />
         Terms of use: <a href="https://www.brainboosterjr.com/terms-of-use">Terms of Use</a>
-      </div>
+      </p>
+            
+              </div>
+          )}
+        </div>
+      )}
     </div>
-  )}
-  </div>
   );
-  };
-  
-  export default App;
+};
+
+export default App;
